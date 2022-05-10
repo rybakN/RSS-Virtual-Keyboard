1226387ad9f618ffacfa7401676e745c432fc1a4
rybakN <dnb4949@gmail.com>
2022-05-10 12:23:29 +0300

    feat: add ESLint module

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "globals": {
+        "window": true,
+        "document": true,
+        "localStorage":true
+      }
+}
